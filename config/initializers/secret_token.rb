c1d679c31818b2700e64b54d6d8a3557da677916
liyicky <liyicky@gmail.com>
2014-12-07 22:18:49 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwilioDemo::Application.config.secret_key_base = '4069d3b564e5e75f27218e6207b1fdfcb32f264cf0e8d99d0e13ea302177dff6aac30907e23b0836fd54a9c597acb37544c533b8d7ff935427abc66d6ec65f05'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwilioDemo::Application.config.session_store :cookie_store, key: '_TwilioDemo_session'
